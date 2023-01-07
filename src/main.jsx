168c84062e2dc41193cf698e22f064e00f9f0ae7
Nikolay Yakushenko <pycckuupc@gmail.com>
2023-01-07 11:32:30 +0300

    navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import { ContextProvider } from './contexts/ContextProvider';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>
+);
